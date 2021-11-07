e6b18cbc539c50263951f819cc1c38ec5e7b6e79
somilror200 <77724072+somilror200@users.noreply.github.com>
2021-11-07 22:35:39 +0530

    - Adding operators functionality - Improvements in the UI - Discarding .0 after the result

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,6 +56,81 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view: View)
+    {
+        if(lastNumeric)
+        {
+            var textValue = textInput?.text.toString()
+            var prefix = ""
+            try
+            {
+                if(textValue.startsWith("-"))
+                {
+                    prefix = "-"
+                    textValue = textValue.substring(1)
+                }
+
+                if(textValue.contains("-"))
+                {
+                    val splitValue = textValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty())
+                    {
+                        one = prefix + one
+                    }
+
+                    textInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+                else if(textValue.contains("+"))
+                {
+                    val splitValue = textValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty())
+                    {
+                        one = prefix + one
+                    }
+
+                    textInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                else if(textValue.contains("/"))
+                {
+                    val splitValue = textValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty())
+                    {
+                        one = prefix + one
+                    }
+
+                    textInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+                else if(textValue.contains("*"))
+                {
+                    val splitValue = textValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty())
+                    {
+                        one = prefix + one
+                    }
+
+                    textInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+
+            }
+            catch(e: ArithmeticException)
+            {
+                e.printStackTrace()
+            }
+        }
+    }
+
     fun isOperatoradded(value: String) : Boolean
     {
         return if(value.startsWith("-"))
@@ -68,5 +144,12 @@ class MainActivity : AppCompatActivity() {
                     || value.contains("+")
                     || value.contains("-")
         }
+    }
+    private fun removeZeroAfterDot(result: String) : String
+    {
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+        return value
     }
 }
